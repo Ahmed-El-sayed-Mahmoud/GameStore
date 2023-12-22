4a97ade506302d00a8152b429b302591658f7d18
Abo Elseed <aelsayed777888@gmail.com>
2023-12-22 06:08:00 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React from 'react';
 import Login from './pages/Login/Login';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from"../src/pages/Home/home"
+import Home from"../src/pages/home/home"
 import SignUp from './pages/SignUp/SignUp';
-function App() {
 
+function App() {
+  console.log(localStorage.getItem("Email"))
   return (
     <BrowserRouter>
       <Routes>
